7cf829614be5ca0af17d3898e832cdd95763ab9d
ADI-SIN <adisin1507@gmail.co>
2017-10-29 16:55:27 +0530

    commit from ADI-SIN

---
+++ README.txt
@@ -0,0 +1,1 @@
+All the html links will be shown of the webpage directed from the entered url.
--- findlinks.py
+++ findlinks.py
@@ -1,7 +1,7 @@
 import urllib2
 import re
 
-url = input("Enter the url:")
+url = input("Enter the URL:")
 #connect to a URL
 website = urllib2.urlopen(url)
 
@@ -11,4 +11,5 @@ html = website.read()
 #use re.findall to get all the links
 links = re.findall('"((http|ftp)s?://.*?)"', html)
 
+#Links will be printed
 print links
